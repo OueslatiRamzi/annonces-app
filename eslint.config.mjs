8ca027fa88004971142ffdad24b799909e460efe
OueslatiRamzi <ramzioueslati10@gmail.com>
2025-04-07 20:22:36 +0100

    Vfl 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
-module.exports = {
-  extends: "next/core-web-vitals",
-  plugins: ["react", "next"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "no-unused-vars": "warn",
-    "@next/next/no-html-link-for-pages": "error",
-    "react/prop-types": "off"
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  }
-};
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
