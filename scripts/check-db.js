e86b6a1444214349836745c7f5dd3eaa8ad13ceb
OueslatiRamzi <ramzioueslati10@gmail.com>
2025-04-18 21:47:27 +0100

    vrrrr

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+// scripts/check-db.js
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
     await prisma.$connect();
-    console.log('✅ Base de données connectée');
+    console.log('✅ Connexion à la base de données réussie !');
   } catch (error) {
-    console.error('❌ Échec de connexion à la base de données');
-    console.error(error);
-    process.exit(1); // <<< bloque le build
+    console.error('❌ Connexion échouée :', error);
+    process.exit(1); // Stop le build
   } finally {
     await prisma.$disconnect();
   }
